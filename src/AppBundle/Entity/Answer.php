259a106cdc65391ea723423a979a6afd1a9f52af
sebastien.harinck <harinck.sebastien@gmail.com>
2016-11-29 20:50:11 +0100

    add Entities : Lesson, Question, Answer

---
+++ src/AppBundle/Entity/Answer.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table(name="answer")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AnswerRepository")
+ */
+class Answer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="correct", type="boolean")
+     */
+    private $correct;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Answer
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set correct
+     *
+     * @param boolean $correct
+     *
+     * @return Answer
+     */
+    public function setCorrect($correct)
+    {
+        $this->correct = $correct;
+
+        return $this;
+    }
+
+    /**
+     * Get correct
+     *
+     * @return bool
+     */
+    public function getCorrect()
+    {
+        return $this->correct;
+    }
+}
+
